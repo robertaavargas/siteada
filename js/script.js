9117a57922fd8921e1d63bc5b2ecd4a06db1a9f9
Roberta Silva <robertaavargas@gmail.com>
2020-12-03 03:17:59 +0000

    Site finalidado!

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
 function validaFormulario(){
-    if(document.getElementById("nome").value != "" &&  document.getElementById("email").value != ""){
+    if(document.getElementById("nome").value != "" &&  document.getElementById("email").value != "" &&
+    document.getElementById("telefone").value != ""){
   alert("Prontinho! Você receberá as novidades por e-mail.")
   }else{
     alert("Por favor, preencha os campos nome e email!")
