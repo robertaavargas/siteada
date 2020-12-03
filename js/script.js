ebe9d4da12fd676e5a8e6248c174389a17528a43
Roberta Silva <robertaavargas@gmail.com>
2020-12-03 01:41:11 +0000

    condicionais: if, else

---
+++ js/script.js
@@ -0,0 +1,6 @@
+if(document.getElementById("nome") estiver preenchido &&  document.getElementById("email") preenchido){
+  envie os dados
+}else{
+  peça para a usuária que preencha os campos nome e e-mail
+}
+
