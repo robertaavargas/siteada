7436e6029aa38ddfa903b2bac2a250a33b9b1fb0
Roberta Silva <robertaavargas@gmail.com>
2020-12-03 01:46:18 +0000

    Valor de dado e string: .value

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-if(document.getElementById("nome") estiver preenchido &&  document.getElementById("email") preenchido){
+if(document.getElementById("nome").value estiver preenchido &&  document.getElementById("email").value preenchido){
   envie os dados
 }else{
   peça para a usuária que preencha os campos nome e e-mail
